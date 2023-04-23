465fb4305a0088260b67dc64db5d2cd8789c87e6
mpr13 <rochama2022@gmail.com>
2023-04-23 13:08:18 -0400

    added files for functions used in app and app itself

---
+++ list_functions.py
@@ -0,0 +1,52 @@
+# Import statistics to use mode
+import statistics
+
+def get_list():
+    # Get input from user for each item
+    
+    
+    # Program stores list instance and prints for user
+    
+    
+    # Program prompts any corrections, if none move on (separate function?)
+    
+    
+    # Create dictionary of items_data
+        # Check if item input in dictionary
+        # If yes, add data to key value set
+    
+    
+    # Based on changes to items_data, update items_prediction dictionary
+
+
+    # Insert list into lists dictionary, date as key and entire list as value
+
+
+
+def check_recent(item):
+    # Find most recent date in "date" of items_data[item]
+
+
+def generate_list():
+    # Get last grocery shopping date, find interval that is next
+    # Get current date and pick date for list closest to today, set as list_date
+
+
+    # Find items that match to interval and day of week
+
+
+    # Print list
+
+
+    # Prompt user for feedback (add or remove items, edit quantities)
+
+
+    # Take info from list and place in items_data to update
+    
+
+
+    """    
+        I would want to eventually add ability to correct data after purchases.
+        On web app, when user opens, prompt --This was our last prediction. Is this accurate to what you purchased?-- Update items_data from there
+        ***How can I replace the values taken from OG list to updated list in dictionary?*** 
+    """
---
+++ test_app.py
@@ -0,0 +1,17 @@
+import list_functions as lf
+import statistics
+
+def main():
+    # Welcome user
+
+    # Ask if input or generate
+
+    # If input 'get_list'
+
+    # If generate 'generate_list'
+
+    # Option to restart (re-prompt for input?)
+    
+
+if __name__ == '__main__':
+    main()
